555658ce9902bac06eebfb92d25a06bece794d73
choiKYeon <gusrudchl12@gmail.com>
2024-01-08 09:26:18 +0900

    1-8

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,14 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {101, 102};
-        List<Integer> list = new ArrayList<>();
-        if (arr.length == 1){
-            arr = new int[]{-1};
+        String answer = "";
+        String s = "abced";
+        String[] str = new String[s.length()];
+        str = s.split("");
+        if (s.length() % 2 != 0){
+            answer = str[s.length()/2];
+        } else {
+            answer = str[s.length()/2] + str[s.length()/2 - 1];
         }
-        for (int i = 0; i < arr.length; i++){
-            list.add(arr[i]);
-        }
-        Arrays.sort(arr);
-        for (int j = 0; j < arr.length; j++){
-            if (list.get(j) == arr[0]){
-                list.remove(list.get(j));
-                break;
-            }
-        }
-        int[] result = new int[list.size()];
-        for (int x = 0; x < list.size(); x++){
-            result[x] = list.get(x);
-        }
-        System.out.println(list);
+        System.out.println(answer);
     }
 }
