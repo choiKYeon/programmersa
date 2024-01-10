a1d8d824d9cca3d6b739d9993163e0b13ff6ad2d
choiKYeon <gusrudchl12@gmail.com>
2024-01-10 17:52:27 +0900

    pro

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] arr1 = {{1}, {2}};
-        int[][] arr2 = {{3}, {4}};
-
-        int[][] answer = new int[arr1.length][arr2.length];
-        for (int i = 0; i < arr1.length; i++){
-            for (int j = 0; j < arr1[i].length; j++){
-                answer[i][j] = arr1[i][j] + arr2[i][j];
-            }
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int i = 1; i <= b; i++){
+            System.out.println("*".repeat(a));
         }
-        System.out.println(answer[0][1]);
     }
 }
