14b5b1a01cceede26ce4f8565b0ec41fb96ecf6c
choiKYeon <gusrudchl12@gmail.com>
2024-01-10 17:42:22 +0900

    sdfa

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
+
 public class Main {
     public static void main(String[] args) {
-        long result = 0;
-        int answer = 0;
-        int price = 3;
-        int money = 20;
-        int count = 4;
-        for (int i = 1; i <= count; i++){
-            answer += price*i;
-        }
-        if (money <= answer){
-            result = answer - money;
-        } else {
-            result = 0;
-        }
-        System.out.println(result);
+        int[][] arr1 = {{1}, {2}};
+        int[][] arr2 = {{3}, {4}};
 
+        int[][] answer = new int[arr1.length][arr2.length];
+        for (int i = 0; i < arr1.length; i++){
+            for (int j = 0; j < arr1[i].length; j++){
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        System.out.println(answer[0][1]);
     }
 }
